8f74cea1bcf118af92bc6f02a6bb69aaed7d4ef3
schabelprojects <schabelprojects@outlook.de>
2023-01-27 16:04:34 +0100

    initial commit

---
+++ ShoppingApp_backend/src/main/java/de/schabelprojects/shoppingapp_backend/ShoppingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package de.schabelprojects.shoppingapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingAppBackendApplication.class, args);
+    }
+
+}
---
+++ ShoppingApp_backend/src/test/java/de/schabelprojects/shoppingapp_backend/ShoppingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schabelprojects.shoppingapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
